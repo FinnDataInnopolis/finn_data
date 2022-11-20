2cf65a193ad2a6dc775a7e2cec08ab714a486428
epetrashko <epetrashko6@yandex.ru>
2022-11-20 22:12:08 +0300

    Fixing linter issues

--- src/api/finnhub/client.tsx
+++ src/api/finnhub/client.tsx
@@ -2,7 +2,7 @@ import { API_KEY } from "./consts";
 
 const finnhub = require("finnhub");
 
-const api_key = finnhub.ApiClient.instance.authentications["api_key"];
-api_key.apiKey = API_KEY;
+const apiKey = finnhub.ApiClient.instance.authentications["api_key"];
+apiKey.apiKey = API_KEY;
 
 export const finnhubClient = new finnhub.DefaultApi();
